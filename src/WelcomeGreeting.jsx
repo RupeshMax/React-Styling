015c19ef8cc2fa9db0ea83c88e693e455d230202
Rubesh K R <rupesh.maxpain@gmail.com>
2023-07-02 12:38:20 +0530

    including react component

---
+++ src/WelcomeGreeting.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WelcomeGreeting() {
+  return <h1> hello! everyone..</h1>;
+}
+
+export default WelcomeGreeting;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-//Show a single h1 that says "Good morning" if between midnight and 12PM.
-//or "Good Afternoon" if between 12PM and 6PM.
-//or "Good evening" if between 6PM and midnight.
-//Apply the "heading" style in the styles.css
-//Dynamically change the color of the h1 using inline css styles.
-//Morning = red, Afternoon = green, Night = blue.
 import React from "react";
 import ReactDOM from "react-dom";
+import WelcomeGreeting from "./WelcomeGreeting";
 
 const customStyle = {
   color: " "
@@ -28,6 +23,7 @@ if (time < 12) {
 
 ReactDOM.render(
   <div>
+    <WelcomeGreeting />
     <h1 className="heading" style={customStyle}>
       {greeting}
     </h1>
